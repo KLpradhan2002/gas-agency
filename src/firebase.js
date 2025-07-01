79655508a9ec5812085f491d3716af60601fa1ce
Kamal lochan pradhan <kamallochanpradhan39@gmail.com>
2025-07-01 21:00:26 +0530

    add env file in git ignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import {getFirestore} from 'firebase/firestore';
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDiSHvywkWLGUI3CHm3VnhjiC41jQEpK0",
-  authDomain: "gas-agency-e3e81.firebaseapp.com",
-  projectId: "gas-agency-e3e81",
-  storageBucket: "gas-agency-e3e81.firebasestorage.app",
-  messagingSenderId: "303918234996",
-  appId: "1:303918234996:web:99dd93773993f791408021"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth= getAuth(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 
